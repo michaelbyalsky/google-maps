38c158da84573b1a655053be1bcb576651ca9f6a
LAPTOP-IDJ82BKU\micha <michael.byalsky@gmail.com>
2020-10-17 13:21:31 +0300

    delete .history

--- .history/client/secret_20201015132011.js
+++
@@ -1,3 +0,0 @@
-export const env = {
-    JS_MAPS_API_KEY: 'AIzaSyBQ2e2GjrvN8oi4090gAWAR5OxnoLDDuRs'
-}
--- .history/client/secret_20201015132422.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    JS_MAPS_API_KEY: 'AIzaSyBQ2e2GjrvN8oi4090gAWAR5OxnoLDDuRs'
-}
