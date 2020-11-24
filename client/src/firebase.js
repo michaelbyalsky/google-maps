c91c7de34fd83d9f37a7238f6af3fb88555353c9
michael byalsky <michael.byalsky@gmail.com>
2020-11-24 11:18:11 +0200

    env fixed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/storage'
 
+console.log(process.env);
 
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -9,7 +10,7 @@ const app = firebase.initializeApp({
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_sAPP_FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 })
